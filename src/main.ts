d3fcf8b9be40dacd75a7c0ef2af333182aa2ef01
Vladimir <user@example.com>
2025-02-19 23:54:28 +0300

    Добавлено задание по теме "Деструктуризация ". "Исправьте код. Объекты".

--- src/main.ts
+++ src/main.ts
@@ -1314,27 +1314,74 @@
 // */
 
 // Исправьте код. Массивы
-const storage = [
-  'nick',
-  'nack',
-  'nock',
-  [
-    {
-      first: 'forecast',
-      child: null,
-    },
-    {
-      first: 'castfore',
-      child: null,
-    },
-    'zzz',
-  ],
-  'no-1',
-  'no-2',
-];
+// const storage = [
+//   'nick',
+//   'nack',
+//   'nock',
+//   [
+//     {
+//       first: 'forecast',
+//       child: null,
+//     },
+//     {
+//       first: 'castfore',
+//       child: null,
+//     },
+//     'zzz',
+//   ],
+//   'no-1',
+//   'no-2',
+// ];
+//
+// const [, , , [, b1, cos], ...nos] = storage;
+//
+// console.log(nos); // [ 'no-1', 'no-2' ]
+// console.log(cos); // 'zzz'
+// console.log(b1); // { first: 'castfore', child: null }
 
-const [, , , [, b1, cos], ...nos] = storage;
+const megaobject = {
+  child: null,
+  child1: {
+    nick: 'name',
+    nicknames: ['n1', 'n2', { n: 'n', b: 'b' }],
+    james: {
+      kates: {
+        nicks: {
+          nicknames: ['nn', 'vv', 'gg'],
+          child: {
+            nick: 'childName',
+            child: undefined,
+          },
+        },
+      },
+    },
+  },
+  child2: {},
+  child3: 'child3',
+};
 
-console.log(nos); // [ 'no-1', 'no-2' ]
-console.log(cos); // 'zzz'
-console.log(b1); // { first: 'castfore', child: null }
+const {
+  child,
+  child1: {
+    nick,
+    nicknames,
+    james: {
+      kates: {
+        nicks: {
+          nicknames: [nicknames1, ...otherNicks],
+          child: { nick: nick1, child: child1 },
+        },
+      },
+    },
+  },
+  ...otherChilds
+} = megaobject;
+
+console.log(child); // null
+console.log(otherChilds); // { child2: {}, child3: 'child3' }
+console.log(nick); // name
+console.log(nicknames); // [ 'n1', 'n2', { n: 'n', b: 'b' } ]
+console.log(nicknames1); // nn
+console.log(otherNicks); // [ 'vv', 'gg' ]
+console.log(nick1); // childName
+console.log(child1); // undefined
